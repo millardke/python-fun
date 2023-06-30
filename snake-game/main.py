b1487fa97d6367cff8a10c6cf88cfdf320858ca9
Kevin Millard <millardke@gmail.com>
2023-06-30 18:59:41 -0500

    Finishing the snake game

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from score import Score
 
 # Game Setup
 screen = Screen()
@@ -12,6 +14,8 @@ screen.tracer(0)
 game_is_on = True
 
 snake = Snake()
+food = Food()
+score = Score()
 
 # Controls
 screen.listen()
@@ -27,8 +31,22 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        score.update_score()
+        snake.extend()
+        food.spawn()
 
+    # Detect collision with wall
+    if abs(snake.head.xcor()) > 280 or abs(snake.head.ycor()) > 280:
+        game_is_on = False
+        score.game_over()
 
+    # Detect collision with self
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 
 screen.exitonclick()
--- snake-game/snake.py
+++ snake-game/snake.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -13,12 +15,18 @@ class Snake:
         self.head = self.segments[0]
 
     def create_snake(self):
-        for segment_index in range(0, 3):
-            segment = Turtle("square")
-            segment.color("white")
-            segment.penup()
-            segment.goto(-20 * segment_index, 0)
-            self.segments.append(segment)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for segment_index in range(len(self.segments) - 1, 0, -1):
